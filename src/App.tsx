af9acf8d0ed45158d536a692c121425381021668
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-10-07 14:27:31 +0200

    0.1 - experimentation possibilités JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const message: string = 'Hello World';
+
+  const handleClick = (name: string) => alert(`Bonjour ${name} !`);
+  const handleClickSansParam = () => alert('Yes papa !');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className='color-pink'>{message}</p>
+      {/* Liaison de l'évènement click avec l'appel de la fonction handleClick avec paramètre */}
+      <button onClick={() => handleClick('Joseph')}>Test Joseph</button>
+      <button onClick={() => handleClick('Coco')}>Test Coco</button>
+      <p>Coucou</p>
+      {/* Liaison de l'évènement click avec l'appel de la fonction handleClickSansParam sans paramètre */}
+      <button onClick={handleClickSansParam}>Un autre boutton</button>
     </div>
   );
-}
+};
 
 export default App;
