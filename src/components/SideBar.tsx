8c818a5deef738d9725c92866987c97aa3955425
Louifine <louis.pluvinage0@gmail.com>
2022-10-15 14:07:53 +0200

    Ajout de la sideBar, liste de plantes et tri par catégories

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+import React, {useState} from 'react';
+import { Plante } from '../pages/Home';
+
+
+interface filterSideBarProps {
+    listElementPlant: Plante[]
+    onChangeCategoriesCheck: { (checkCategories: string[]): void}
+}
+
+const SideBar = ({listElementPlant,onChangeCategoriesCheck}: filterSideBarProps) => {
+
+    const categories = _.uniq(listElementPlant.map((plante)=> plante.category))
+    const [checkCategories, setCheckCategories] = useState<string[]>([])
+    
+    function handleCheck (e: React.ChangeEvent<HTMLInputElement>){
+        let tab:string[]= []
+        if(e.currentTarget.checked){
+            tab = [...checkCategories, e.currentTarget.value]
+        }
+        else {
+            tab = [...checkCategories.filter((x)=>x !== e.currentTarget.value)]
+        }
+        setCheckCategories(tab)
+        onChangeCategoriesCheck(tab)
+    }
+
+    return (
+    <div className='custom-side-bar flex-shrink-0 bg-white border-end'>
+      <div className='p-3 border-bottom'>
+        <span className='fs-5 fw-semibold'>Filtres</span>
+      </div>
+      <ul className='list-unstyled ps-0'>
+        <div className='p-3'>
+          <p className='mb-1 fs-5 fw-semibold'>Catégories</p>
+            {categories.map((cat)=> 
+            <div>
+                <input type="checkbox" value={cat} id={cat} onChange={handleCheck}/>
+                <label htmlFor={cat}>{cat}</label>
+            </div>
+            )
+            }
+        </div>
+      </ul>
+      
+    </div>);
+}
+
+export default SideBar
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,37 @@
+import {list_products} from '../data'
+import SideBar from '../components/SideBar'
+import { useState } from 'react';
+
+export interface Plante {
+  id: string;
+  name: string;
+  unitprice_ati: number;
+  quantity: number;
+  category: string;
+  rating: number;
+  url_picture: string;
+}
+
+const listePlantes: Plante[] = list_products;
+
 const Home = () => {
-  return <div>Page d'accueil</div>;
+
+const [selectCategories, setSelectCategories] = useState<string[]>([])
+let results = [...listePlantes];
+
+if(selectCategories.length>0)
+{
+  results = results.filter(x => selectCategories.includes(x.category))
+}
+
+
+  return (
+  <div className='d-flex align-items-stretch'>
+    <SideBar listElementPlant={listePlantes} onChangeCategoriesCheck={mesDataCheck => setSelectCategories(mesDataCheck)}/>
+    <div className='container-fluid custom-main'> 
+      {results
+      .map((plante) => <li>{plante.name} - {plante.category} - 💵 {plante.unitprice_ati}€ - ⭐{plante.rating}</li>)} </div>
+    </div>
+  );
 };
 export default Home;
